ad5432528da455e1d6a93ff302005f6af1498cea
darka <luca@ahd-creative.com>
2022-09-08 11:54:14 +0200

    Ads CSS

--- functions.php
+++ functions.php
@@ -3,5 +3,10 @@ function hello_elementor_child_enqueue(): void
 {
     wp_enqueue_style('hello-elementor-child-theme', get_stylesheet_directory_uri() . '/style.css', ['hello-elementor-theme-style'], '1.0', 'all');
 }
+function xivita_ad_css_enqueue(): void
+{
+	wp_enqueue_style('xivita-ad-css', get_stylesheet_directory_uri() . '/ads.css', ['hello-elementor-child-theme'], '1.2', 'all');
+}
 
 add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue', 30);
+add_action('wp_enqueue_scripts','xivita_ad_css_enqueue', 99);
