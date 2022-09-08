491c669c35182059bb49fb1e14856ec3a770bab4
darka <luca@ahd-creative.com>
2022-09-08 12:40:21 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,5 +8,11 @@ function xivita_ad_css_enqueue(): void
 	wp_enqueue_style('xivita-ad-css', get_stylesheet_directory_uri() . '/ads.css', ['hello-elementor-child-theme'], '1.2', 'all');
 }
 
+function xivita_css_enqueue(): void
+{
+	wp_enqueue_style('xivita-authors', get_stylesheet_directory_uri() . '/assets/css/authors.css', ['xivita-ad-css'], '1.2', 'all');
+}
+
 add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue', 30);
 add_action('wp_enqueue_scripts','xivita_ad_css_enqueue', 99);
+add_action('wp_enqueue_scripts', 'xivita_css_enqueue', 99);
